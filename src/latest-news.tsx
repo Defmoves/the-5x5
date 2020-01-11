2314d557652a25d045d4353f0675b826aeb36c54
José Des Sousa <defmoves@pm.me>
2020-01-11 17:53:10 +0000

    added basic list of nation buttons

--- src/latest-news.tsx
+++ src/latest-news.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { FunctionComponent } from "react";
 import { createStyles, makeStyles, Theme } from "@material-ui/core/styles";
 import Grid from "@material-ui/core/Grid";
 
@@ -6,8 +6,21 @@ const useStyles = makeStyles((theme: Theme) =>
   createStyles({
     root: {
       padding: theme.spacing(2)
+    },
+    listItem: {
+      padding: theme.spacing(1)
     }
   })
+);
+
+interface buttonProps {
+  onClick?: () => void;
+  name?: string;
+}
+
+// TODO: replace this with a more elegant interface
+const Button: FunctionComponent<buttonProps> = ({ onClick, name }) => (
+  <button onClick={onClick}>{name}</button>
 );
 
 export default function ButtonAppBar() {
@@ -17,7 +30,23 @@ export default function ButtonAppBar() {
     <div className={classes.root}>
       <Grid container spacing={3}>
         <Grid item xs={3}>
-          Source Select
+          <ul>
+            <li className={classes.listItem}>
+              <Button name="United Kingdom" />
+            </li>
+            <li className={classes.listItem}>
+              <Button name="United States" />
+            </li>
+            <li className={classes.listItem}>
+              <Button name="France" />
+            </li>
+            <li className={classes.listItem}>
+              <Button name="Australia" />
+            </li>
+            <li className={classes.listItem}>
+              <Button name="India" />
+            </li>
+          </ul>
         </Grid>
         <Grid item xs={9}>
           Latest News
