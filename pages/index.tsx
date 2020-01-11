1f987044dd09910a67c3ddfdef947e596089eabb
José Des Sousa <defmoves@pm.me>
2020-01-11 17:04:32 +0000

    added basic header

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,33 @@
 import React from "react";
-import Container from "@material-ui/core/Container";
+import { createStyles, makeStyles, Theme } from "@material-ui/core/styles";
+import AppBar from "@material-ui/core/AppBar";
+import Typography from "@material-ui/core/Typography";
 
-export default function Index() {
-  return <Container fixed></Container>;
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      flexGrow: 1
+    },
+    header: {
+      padding: theme.spacing(3),
+      textAlign: "center"
+    },
+    title: {
+      flexGrow: 1
+    }
+  })
+);
+
+export default function ButtonAppBar() {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.root}>
+      <AppBar position="static" className={classes.header}>
+        <Typography variant="h6" className={classes.title}>
+          The 5x5
+        </Typography>
+      </AppBar>
+    </div>
+  );
 }
