42b4f2f47df7f651949135eb500b597a03484f66
José Des Sousa <defmoves@pm.me>
2020-01-12 17:20:21 +0000

    added new font to header title

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { createStyles, makeStyles, Theme } from "@material-ui/core/styles";
 import AppBar from "@material-ui/core/AppBar";
-import Typography from "@material-ui/core/Typography";
 import Container from "@material-ui/core/Container";
 
 import LatestNews from "../src/latest-news/";
@@ -12,9 +11,11 @@ const useStyles = makeStyles((theme: Theme) =>
       flexGrow: 1
     },
     header: {
-      padding: theme.spacing(3),
       marginBottom: theme.spacing(3),
-      textAlign: "center"
+      textAlign: "center",
+      fontFamily: "'Playfair Display SC', serif",
+      fontSize: 18,
+      fontWeight: 700
     },
     title: {
       flexGrow: 1
@@ -28,9 +29,7 @@ export default function App() {
   return (
     <div className={classes.root}>
       <AppBar position="static" className={classes.header} color={"secondary"}>
-        <Typography variant="h3" className={classes.title}>
-          The 5x5
-        </Typography>
+        <h1 className={classes.title}>The 5x5</h1>
       </AppBar>
       <Container fixed>
         <LatestNews />
